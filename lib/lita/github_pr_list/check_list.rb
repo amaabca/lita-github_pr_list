416d8bf3ced73db033c8bc2e7d8314b8678f0386
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-08-01 12:27:36 -0600

    Add check list to comment on pr
    * when a pr is open a check list markup will
    be added to the text on the pr body comment.

--- lib/lita/github_pr_list/check_list.rb
+++ lib/lita/github_pr_list/check_list.rb
@@ -1,7 +1,7 @@
 module Lita
   module GithubPrList
     class CheckList
-      attr_accessor :request, :response, :payload, :redis
+      attr_accessor :request, :response, :payload, :redis, :issue_body
 
       def initialize(params = {})
         self.response = params.fetch(:response, nil)
@@ -10,13 +10,28 @@ module Lita
 
         raise "invalid params in #{self.class.name}" if response.nil? || request.nil? || redis.nil?
 
+        list = "- [ ] Change log
+                - [ ] Demo page
+                - [ ] Product owner signoff
+                - [ ] Merge into master
+                - [ ] deploy to production "
+
         # https://developer.github.com/v3/activity/events/types/#issuecommentevent
         self.payload = JSON.parse(request.body.read)
-
-
+        self.issue_body = "#{payload["pull_request"]["body"]} #{list}"
+        self.issue_id = payload["number"]
+        self.issue_owner = payload["pull_request"]["user"]["login"]
+        self.issue_title = payload["pull_request"]["base"]["name"]
+        self.issue_html_url = payload["pull_request"]["issue_url"]
       end
 
-
+      def message
+        if payload[action] == "opened" do
+          #PATCH /repos/:owner/:repo/issues/comments/:id
+          response = RestClient.post "/repos/#{issue_owner}/#{issue_title}/issues/comments/#{issue_id}", body: issue_body
+          return nil if response.status != 200
+          "@#{issue_owner} check list added to your pull request: #{issue_title} has passed. #{issue_html_url}"
+        end
     end
   end
 end
