85da842478997718df039b2871b6bd5b10ecd3ae
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-09-10 14:55:24 -0600

    Fixed test
    * Test was missing a fixture
    * Change fistures files name to have a better relation with the
    test file.

--- spec/lita/handlers/check_list_spec.rb
+++ spec/lita/handlers/check_list_spec.rb
@@ -4,26 +4,21 @@ describe Lita::Handlers::GithubPrList, lita_handler: true do
     Lita.config.handlers.github_pr_list.github_access_token = 'wafflesausages111111'
   end
 
-  let(:pull_request_edit_comment) { File.read("spec/fixtures/pull_request_edit_comment.json") }
-  let(:pull_request_response) { [File.read("spec/fixtures/edit_comment.json")] }
-  let(:edit_pull_request_response) { Rack::Response.new(pull_request_response, 200, { 'Content-Type' => 'json' }) }
+  let(:update_pull_request_response) { File.read("spec/fixtures/update_pull_request_response.json") }
+  let(:open_pull_request_response) { [File.read("spec/fixtures/open_pull_request_response.json")] }
+  #let(:open_pull_request_rack_response) { Rack::Response.new(open_pull_request_response, 200, { 'Content-Type' => 'json' }) }
   let(:check_list) do
-    "- [ ] Change log
-    - [ ] Demo page
-    - [ ] Product owner signoff
-    - [ ] Merge into master
-    - [ ] Deploy to production"
+    "- [ ] Change log - [ ] Demo page - [ ] Product owner signoff - [ ] Merge into master - [ ] Deploy to production"
   end
 
   it { routes_http(:post, "/check_list").to(:check_list) }
 
-  it "mentions the github user in the room and tell them the check list was added to the pull request" do
-    allow_any_instance_of(Octokit::Client).to receive(:update_pull_request).and_return(edit_pull_request_response)
-    request = Rack::Request.new("rack.input" => StringIO.new(pull_request_review_comment))
+  it "adds the check list to the body of the pull request" do
+    allow_any_instance_of(Octokit::Client).to receive(:update_pull_request).and_return(update_pull_request_response)
+    request = Rack::Request.new("rack.input" => StringIO.new(open_pull_request_response.first))
     response = Rack::Response.new(['Hello'], 200, {'Content-Type' => 'text/plain'})
-
     github_handler = Lita::Handlers::GithubPrList.new
     github_handler.check_list(request, response)
-    expect(edit_pull_request_response.body.first).to include(check_list)
+    expect(update_pull_request_response).to include(check_list)
   end
 end
